1719e821791b1eae1c27f8ab7bbe3eab8bae9caa
tamlinlove <tamlinlollislove@gmail.com>
2019-06-24 13:44:28 +0200

    Webcam works

--- webcam.py
+++ webcam.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -9,7 +9,7 @@ while(True):
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     array_values = np.array(frame)
-    print(array_values.shape)
+    #print(array_values.shape)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
